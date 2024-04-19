86c97ad1a645a3876acf29c7376ef505eb161830
soapBall <167486877+soapBall@users.noreply.github.com>
2024-04-18 20:32:13 -0500

    hi

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var numOfClicks = 0;
+var hedding = document.createElement("h1")
 var para = document.createElement("p");
+
 randomBtm.addEventListener("click", function(e){
   numOfClicks += 1
   if (numOfClicks==1) {
@@ -20,5 +22,6 @@ randomBtm.addEventListener("click", function(e){
 
 
 function fact1(){
-  para.innerText = "Why is the time we use base 60?<br>Have you ever wondered why time measurement for almost all of the world is in base 60, instead of base 10 like basically everything else?";
+  para.innerText = "Have you ever wondered why time measurement for almost all of the world is in base 60, instead of base 10 like basically everything else?";
+  hedding.innerText = "Why is the time we use base 60?"
 };
