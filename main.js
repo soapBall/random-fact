13819dde564cbecdc575759e9d44e2af5e3fd454
soapBall <167486877+soapBall@users.noreply.github.com>
2024-04-18 20:24:23 -0500

    stuff

---
+++ main.js
@@ -0,0 +1,24 @@
+var numOfClicks = 0;
+var para = document.createElement("p");
+randomBtm.addEventListener("click", function(e){
+  numOfClicks += 1
+  if (numOfClicks==1) {
+    para.innerText = "This is a paragraph";
+  } else {
+
+    fact1()
+  };
+  
+  
+  console.log("I was clicked!!");
+
+  
+
+  document.body.appendChild(para);
+});
+
+
+
+function fact1(){
+  para.innerText = "Why is the time we use base 60?<br>Have you ever wondered why time measurement for almost all of the world is in base 60, instead of base 10 like basically everything else?";
+};
