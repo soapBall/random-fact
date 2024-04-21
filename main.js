721b2d6c019c73d2c184c107b6d356cb42bd0633
Blake <balwashburn@gmail.com>
2024-04-21 12:06:08 -0500

    hi

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ var random = 0
 const head = document.createElement("h1");
 const para = document.createElement("p");
 const facts = ["A while ago, people invented washing machines that use WiFi. Why, no one knows. But these could destroy the world because of AI. What if someone made a true AI, an AI that could have its own ideas? That AI, if it had bad intentions, could hypothetically hack into the internet, control the washing machines, and flood full houses! Meanwhile, another AI that went into the internet and took over one of those washing machine maker robots could make millions of more WiFi washing machines and convince everyone that they are the best washing machines. The world would be destroyed!"
-,"Have you ever wondered why time measurement for almost all of the world is in base 60, instead of base 10 like basically everything else? The clock we use dates back to"];
+,"Have you ever wondered why time measurement for almost all of the world is in base 60, instead of base 10 like basically everything else? The clock we use dates back to",
+'Apple product names often start with the letter “i” before their name because Steve Jobs said in 1998, the “i” in the iMac stood for “internet”, but he also said that it could stand for "individual," "instruct," "inform," and "inspire". Later he stated that the “i” in front of a product like the iPhone, iMac, and iPad did not have any official meaning, and it can mean whatever you want it to be. Although we know what it could mean, it still leaves us with the question, why did they choose the letter “i” instead of a letter like “p” for possibility?'];
 const hFacts = ["Why AI will destroy your washing machines"
-, "Why is the time we use in base 60?"];
+, "Why is the time we use in base 60?",
+"Why do most apple products have an “i” in front of their name?"];
 var factsSeen=[]
-var prevousNum = null
+var lastFact
 randomBtm.addEventListener("click", function(e){
   numOfClicks += 1
   randomNum = Math.round(((Math.random(0)*(facts.length-1))));
   if (factsSeen.length>=facts.length){
     factsSeen = [];
     console.log("list was emptied!")
-    factsSeen.push(prevousNum)
+    factsSeen.push(lastFact)
+
   };
 
   while (factsSeen.includes(randomNum)){
@@ -27,6 +30,5 @@ randomBtm.addEventListener("click", function(e){
   console.log("I was clicked!!");
   document.body.appendChild(head);
   document.body.appendChild(para);
-  prevousNum=randomNum
-
+  lastFact=randomNum
 });
